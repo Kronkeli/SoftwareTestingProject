f72972652db002489add7d22da283dd3d5099fdb
Samu Salko <samusalko@Samu-MacBook-Pro-3.local>
2020-12-07 12:09:08 +0200

    Got the coverage reporting working with c8. Added script to pipe it to coveralls.

--- test/UnitTests/addTest.js
+++ test/UnitTests/addTest.js
@@ -1,21 +1,17 @@
 import chai from "chai"
 import add from "../../src/add.js"
 
-describe('add', function () {
+describe('add Tests', function () {
     it('Add should return sum of two positive numbers', function () {
         chai.assert.equal(add(2054, 9999), 2054 + 9999);
     });
-});
-
-describe('add should return sum of two negative numbers', function () {
-    it('', function () {
+    it('add returns sum of two negative numbers', function () {
         chai.assert.equal(add(-5, -7), -5 + (-7));
     });
-});
-
-describe('add should return zero of zum of two zeros', function () {
-    it('', function () {
+    it('add works with zeros', function () {
         chai.assert.equal(add(0, 0), 0);
     });
+    it('add works with numbers in string form', function () {
+        chai.assert.equal(add('1', '4'), 5)
+    })
 });
-
